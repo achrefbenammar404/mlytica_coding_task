07820059587ab370d1a73be388c903a8cd49594b
achrefbenammar546372819 <achref.benammar404@gmail.com>
2024-05-14 03:26:51 +0100

    feat : logging improved

--- api_service/app/main.py
+++ api_service/app/main.py
@@ -4,75 +4,103 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from retrieve_important_parts import InformationExtractor 
-from time import sleep 
+from time import sleep
+import os
+import logging
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 
+# Set up basic configuration for logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Create FastAPI app instance
 app = FastAPI()
+
+# Retrieve sensitive data from environment variables
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY_CHAT")
+AZURE_ENDPOINT = os.getenv("AZURE_ENDPOINT_CHAT")
+AZURE_DEPLOYMENT = os.getenv("AZURE_DEPLOYMENT_CHAT")
+
+# Define path and create an instance of InformationExtractor
 path = "competition_results.txt"
 information_extractor = InformationExtractor()
 information_extractor.add_document(document_path=path)
-#data_manager = DataManager()
 
+# Initialize language model
 llm = AzureChatOpenAI(
-            openai_api_key="cd8a4aa317b9451b91d4e991df77b8ed",
-            azure_endpoint="https://frfraoaikymchat0005.openai.azure.com/",
-            azure_deployment="kymgpt35",
+            openai_api_key=OPENAI_API_KEY,
+            azure_endpoint=AZURE_ENDPOINT,
+            azure_deployment=AZURE_DEPLOYMENT,
             api_version="2023-12-01-preview",
             temperature=0
 )
 
-messages = []
-
-
 @app.get("/get_department/{placement}")
-def get_department(placement: str)->str:
-    while(information_extractor.running ):
-        sleep(1)
-    chain = create_langchain_chain()
-    context , question  = get_context_question("department" , placement=placement)
-    return chain.invoke({"question" : question  , 'chunks' : context})
-
+def get_department(placement: str) -> str:
+    """
+    Retrieve the department associated with a specific placement from competition results.
+    """
+    try:
+        while information_extractor.running:
+            sleep(1)
+        chain = create_langchain_chain()
+        context, question = get_context_question("department", placement=placement)
+        return chain.invoke({"question": question, 'chunks': context})
+    except Exception as e:
+        logging.error(f"Error in get_department: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @app.get("/get_employee/{placement}")
-def get_employee(placement: str)->str:
-    while(information_extractor.running ):
-        sleep(1)
-    chain = create_langchain_chain()
-    context , question  = get_context_question("employee" , placement=placement)
-    return "employee"
-    return chain.invoke({"question" : question  , 'chunks' : context})
+def get_employee(placement: str) -> str:
+    """
+    Retrieve the employee associated with a specific placement from competition results.
+    """
+    try:
+        while information_extractor.running:
+            sleep(1)
+        chain = create_langchain_chain()
+        context, question = get_context_question("employee", placement=placement)
+        return chain.invoke({"question": question, 'chunks': context})
+    except Exception as e:
+        logging.error(f"Error in get_employee: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
-
-
-def create_langchain_chain () : 
+def create_langchain_chain():
+    """
+    Create a language processing chain to handle incoming queries and context.
+    """
     prompt_message = (
         """
-        you are an AI assistant that can extract information from context extracted from various documents. 
-        you will be given a context that will contain information relevant to the question you are being tasked. Your task is to answer the question based only on the snippets of the documnt given to you, if you don't find the answer in the snippets you can repky by "answer not found in the snippets given to me".
-        this is the question given to you : {question}
-        these are the snippets based on whoch you will answer the question : {chunks}.
+        You are an AI assistant that can extract information from context extracted from various documents. 
+        You will be given a context that will contain information relevant to the question you are being tasked. Your task is to answer the question based only on the snippets of the document given to you, if you don't find the answer in the snippets you can reply by "answer not found in the snippets given to me".
+        This is the question given to you: {question}
+        These are the snippets based on which you will answer the question: {chunks}.
         Your answer should be entirely based on the snippets given to you. 
-    """)
+        """
+    )
     prompt = ChatPromptTemplate.from_template(prompt_message)
     
     llm = AzureChatOpenAI(
-        openai_api_key="cd8a4aa317b9451b91d4e991df77b8ed",
-        azure_endpoint="https://frfraoaikymchat0005.openai.azure.com/",
-        azure_deployment="kymgpt35",
+        openai_api_key=OPENAI_API_KEY,
+        azure_endpoint=AZURE_ENDPOINT,
+        azure_deployment=AZURE_DEPLOYMENT,
         api_version="2023-12-01-preview",
         temperature=0
     )
     
     parser = StrOutputParser()
     
-    return {"chunks": RunnablePassthrough(), "question": RunnablePassthrough() } | prompt | llm |parser  
+    return RunnablePassthrough() | prompt | llm | parser
 
-
-def get_context_question(element_questioned : str , placement : int  ) -> str : 
-    if (element_questioned == "department") : 
-        question = f"What is the {element_questioned} with the placement : {placement} in this competition?"
-    elif (element_questioned == "employee") : 
+def get_context_question(element_questioned: str, placement: int) -> (str, str):
+    """
+    Generate the context and question for the given element and placement.
+    """
+    if element_questioned == "department":
+        question = f"What is the {element_questioned} with the placement: {placement} in this competition?"
+    elif element_questioned == "employee":
         question = f"Who is the {element_questioned} with the placement {placement} in this competition?"
-
     context = information_extractor.extract_relevant_info_rag(task=question)
-    return context , question
+    return context, question
--- api_service/app/retrieve_important_parts.py
+++ api_service/app/retrieve_important_parts.py
@@ -1,26 +1,38 @@
 import os
-import json
 import logging
-import requests
 from langchain_chroma import Chroma
 from langchain_openai.embeddings.azure import AzureOpenAIEmbeddings
 from langchain_community.document_loaders.text import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-
 # Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class InformationExtractor:
+    """
+    A class dedicated to extracting and retrieving information from documents using vector embeddings.
+    
+    Attributes:
+        embeddings (AzureOpenAIEmbeddings): Azure embeddings used for document vectorization.
+        k (int): Number of relevant documents to retrieve.
+        documents_paths (list): Paths to the documents loaded.
+        retriever (Chroma): Document retriever based on vector embeddings.
+        vectorstore (Chroma): Store of document vectors.
+        running (bool): Flag indicating if document processing is ongoing.
+        indexed (list): List of indexed document paths.
+    """
+
     embeddings = AzureOpenAIEmbeddings(
-        azure_deployment="kymadaembedding",
-        azure_endpoint="https://frfraoaikymchat0005.openai.azure.com/",
-        api_key="cd8a4aa317b9451b91d4e991df77b8ed",
+        azure_deployment=os.getenv("AZURE_DEPLOYMENT_EMBED"),
+        azure_endpoint=os.getenv("AZURE_ENDPOINT_EMBED"),
+        api_key=os.getenv("AZURE_API_KEY_EMBED"),
         api_version="2023-12-01-preview"
     )
 
     def __init__(self, k=15):
+        """
+        Initializes the InformationExtractor with a specified number of documents to retrieve (k).
+        """
         self.documents_paths = []
         self.retriever = None
         self.vectorstore = None
@@ -28,19 +40,34 @@ class InformationExtractor:
         self.running = False
         self.indexed = []
 
-    def extract_relevant_info_rag(self, task : str ) -> str :
+    def extract_relevant_info_rag(self, task: str) -> str:
+        """
+        Retrieves relevant information based on the given task query from the loaded documents.
+        
+        Parameters:
+            task (str): The query or task description to find relevant documents for.
+
+        Returns:
+            str: A concatenated string of the top relevant document chunks or an error message.
+        """
         if self.retriever is None or self.vectorstore is None:
-            logger.warning("No documents uploaded for reading. Please upload files first.")
+            logging.warning("Retrieval attempted before documents were properly uploaded and indexed.")
             return "No documents uploaded to be read. You or your co-worker (information provider) need to use the upload user file tool to get relevant files from the user."
 
         try:
             list_documents = self.retriever.get_relevant_documents(task)
-            return '----------------------------\n\n'.join([f"chunk Number {i}: {doc.page_content}" for i, doc in enumerate(list_documents)])
+            return '----------------------------\n\n'.join([f"Chunk Number {i}: {doc.page_content}" for i, doc in enumerate(list_documents)])
         except Exception as e:
-            logger.error(f"Error retrieving relevant documents: {e}")
+            logging.error(f"Error retrieving relevant documents: {e}")
             return "Error occurred while retrieving documents."
 
-    def add_document(self, document_path : str ) -> None :
+    def add_document(self, document_path: str) -> None:
+        """
+        Processes and adds a document from the given path to the vector store for future retrieval tasks.
+
+        Parameters:
+            document_path (str): The file path of the document to be processed and added.
+        """
         self.running = True
         try:
             self.documents_paths.append(document_path)
@@ -48,7 +75,7 @@ class InformationExtractor:
             docs = loader.load()
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
             splits = text_splitter.split_documents(docs)
-            logger.info(f"Loading document from path: {document_path}")
+            logging.info(f"Loading document from path: {document_path}")
 
             if self.vectorstore is None:
                 self.vectorstore = Chroma.from_documents(documents=splits, embedding=InformationExtractor.embeddings)
@@ -57,12 +84,8 @@ class InformationExtractor:
 
             self.retriever = self.vectorstore.as_retriever(search_kwargs={'k': self.k})
             self.indexed.append(document_path)
-            logger.info(f"upload of file {document_path} successful")
+            logging.info(f"Document '{document_path}' successfully uploaded and indexed.")
         except Exception as e:
-            logger.error(f"Error adding document '{document_path}': {e}")
+            logging.error(f"Error adding document '{document_path}': {e}")
         finally:
             self.running = False
-
-
-
-
